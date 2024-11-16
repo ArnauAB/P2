7e3dcc5b9cba642bd681fa112c6f9be2e16cc007
adriuus2000 <adrian.lopez.pardina@estudiantat.upc.edu>
2024-11-17 00:19:40 +0100

    Update main_vad.c

--- src/main_vad.c
+++ src/main_vad.c
@@ -98,10 +98,14 @@ int main(int argc, char *argv[]) {
       }
     }
 
-    if (sndfile_out != 0) {
+    if (sndfile_out != 0 && state==ST_SILENCE) {
       /* TODO: go back and write zeros in silence segments */
+      sf_seek(sndfile_out, -frame_size, SEEK_CUR);
+      sf_write_float(sndfile_out, buffer_zeros, frame_size);
+      last_state = state;
     }
   }
+
 
   state = vad_close(vad_data);
   /* TODO: what do you want to print, for last frames? */
